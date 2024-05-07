5303c3ed247548e87a4d5eeac1e0d67603e4b71a
duycan17 <duydk.22git@vku.udn.vn>
2024-05-07 19:48:16 +0700

    upload code

--- src/main/java/org/example/UDPClient.java
+++ src/main/java/org/example/UDPClient.java
@@ -1,2 +1,24 @@
-package org.example;public class UDPClient {
+package org.example;
+
+import java.net.*;
+
+public class UDPClient {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            String message = "Hello, UDP Server!";
+            byte[] sendData = message.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 12345);
+            socket.send(sendPacket);
+            System.out.println("Message sent to server.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
 }
--- src/main/java/org/example/UDPServer.java
+++ src/main/java/org/example/UDPServer.java
@@ -1,2 +1,23 @@
-package org.example;public class UDPServer {
+package org.example;
+
+import java.net.*;
+
+public class UDPServer {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket(12345);
+            byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + receivedMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
 }
